a6ccac8af9ad710c6e7cf530759bf3009f5be22f
Jefferey Huang <jeffereyhuang@gmail.com>
2019-04-27 15:04:19 -0400

    markdown, aggregating scores

--- script.R
+++ script.R
@@ -1,33 +1,91 @@
 library(tidyverse)
 library(readxl)
+library(janitor)
 
+sat18 <- read_xls("sat18.xls") %>% 
+  clean_names() %>% 
+  select(-year)
 
-sat18 <- read_xls("sat18.xls")
-sat17 <- read_xls("sat17.xls")
-sat16 <- read_xls("sat16.xls")
-sat15 <- read_xls("sat15.xls")
+sat17 <- read_xls("sat17.xls") %>% 
+  clean_names()
 
-full_sat <- bind_rows(sat18, sat17, sat16, sat15, .id="year")
+sat16 <- read_xls("sat16.xls") %>% 
+  clean_names() %>% 
+  select(-year)
 
-act18 <- read_xls("act18.xls")
-act17 <- read_xls("act17.xls")
-act16 <- read_xls("act16.xls")
+sat15 <- read_xls("sat15.xls") %>% 
+  clean_names()
+
+full_sat <- bind_rows(sat15, sat16, sat17, sat18, .id="year") %>% 
+  drop_na(sname)
+
+act18 <- read_xls("act18.xls") %>% 
+  clean_names() %>% 
+  mutate_at(9:14, as.integer) %>% 
+  select(-year)
+
+act17 <- read_xls("act17.xls") %>% 
+  clean_names() %>% 
+  mutate_at(9:14, as.integer)
+
+act16 <- read_xls("act16.xls") %>% 
+  clean_names() %>% 
+  mutate_at(6:11, as.integer)
+
 act15 <- read_xls("act15.xls") %>% 
-  mutate(Enroll12 )
+  clean_names() %>% 
+  mutate_at(6:11, as.integer) %>% 
+  select(-year)
 
-full_act <- bind_rows(act18, act17, act16, act15, .id="year")
+full_act <- bind_rows(act15, act16, act17, act18, .id="year") %>% 
+  drop_na(sname)
 
-ap18 <- read_xls("ap18.xls")
-ap17 <- read_xls("ap17.xls")
-ap16 <- read_xls("ap16.xls")
-ap15 <- read_xls("ap15.xls")
+ap18 <- read_xls("ap18.xls") %>% 
+  clean_names() %>% 
+  select(-year) %>% 
+  mutate_at(9:16, as.integer)
 
-expense18 <- read_xlsx("expense18.xlsx")
-expense17 <- read_xlsx("expense17.xlsx")
-expense16 <- read_xls("expense16.xls")
-expense15 <- read_xls("expense15.xls")
+ap17 <- read_xls("ap17.xls") %>% 
+  clean_names() %>% 
+  mutate_at(9:16, as.integer)
+
+ap16 <- read_xls("ap16.xls") %>% 
+  clean_names() %>% 
+  select(-year) %>% 
+  mutate_at(6:13, as.integer)
+ap15 <- read_xls("ap15.xls") %>% 
+  clean_names() %>% 
+  select(-year) %>% 
+  mutate_at(6:13, as.integer)
+
+full_ap <- bind_rows(ap15, ap16, ap17, ap18, .id="year") %>% 
+  drop_na(sname)
+
+expense18 <- read_xlsx("expense18.xlsx",
+                       skip = 10) %>% 
+  mutate(CDS = as.character(CDS))
+expense17 <- read_xlsx("expense17.xlsx",
+                       skip = 8)
+expense16 <- read_xls("expense16.xls", 
+                      skip = 7)
+expense15 <- read_xls("expense15.xls",
+                      skip = 7)
+
+full_expense <- bind_rows(expense15, expense16, expense17, expense18, .id="year")
 
 
+# dist_seg <- read_xlsx("district_seg.xlsx") %>% 
+#   filter(str_detect(metroname, "CA")) %>% 
+#   group_by(metroname) %>%
+#   summarize() %>%
+#   View()
+
+# natl_seg <- read_xlsx("national-school-segregation-web.xlsx") %>% 
+#   clean_names() %>% 
+#   # filter(str_detect(metro_area, "CA")) %>% 
+#   # group_by(metro_area) %>% 
+#   # summarize() %>% 
+#   # View()
 
 
 
